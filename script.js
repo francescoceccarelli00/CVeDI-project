54494f9abdef24beb6ee565ca1c7073cb20f370b
Francesco Ceccarelli <frasco2000@gmail.com>
2024-01-17 13:06:58 +0100

    v2-cvedi

---
+++ script.js
@@ -0,0 +1,12 @@
+let prevScrollpos = window.scrollY; // GESTIONE NAVBAR ALLO SCROLL 
+    window.onscroll = function() {
+        let currentScrollPos = window.scrollY;
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("navbar").style.top = "0";
+        } else {
+            document.getElementById("navbar").style.top = "-126px"; /* Altezza della barra di navigazione */
+        }
+        prevScrollpos = currentScrollPos;
+    };
+
+VanillaTilt.init(document.querySelector(".envelope"),{}); //CHIAMANDO VANILLA TILT SI DA L'EFFETTO ALL'ELEMENTO CON LA CLASSE SELEZIONATA
