d098a99378fa01ded0def0c708147920cd6783d8
Francesco Ceccarelli <frasco2000@gmail.com>
2024-03-22 11:28:01 +0100

    altre cose

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let prevScrollpos = window.scrollY; // GESTIONE NAVBAR ALLO SCROLL 
     window.onscroll = function() {
         let currentScrollPos = window.scrollY;
-        if (prevScrollpos > currentScrollPos) {
+        if (prevScrollpos > (currentScrollPos)) {
             document.getElementById("navbar").style.top = "0";
         } else {
             document.getElementById("navbar").style.top = "-126px"; /* Altezza della barra di navigazione */
@@ -9,15 +9,17 @@ let prevScrollpos = window.scrollY; // GESTIONE NAVBAR ALLO SCROLL
         prevScrollpos = currentScrollPos;
     };
 
-VanillaTilt.init(document.querySelector(".ink"),{
-    max: 8,
-	speed: 300
-});
 VanillaTilt.init(document.querySelector(".quill"),{
-    max: 2,
+    max: 10,
 	speed: 1000
 });
+
 VanillaTilt.init(document.querySelector(".envelope"),{
+    max: 15,
+    speed: 1500
+});
+
+VanillaTilt.init(document.querySelector(".glass"),{
     max: 15,
     speed: 1500
 }); //CHIAMANDO VANILLA TILT SI DA L'EFFETTO ALL'ELEMENTO CON LA CLASSE SELEZIONATA
